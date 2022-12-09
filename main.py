8e856a8ca6fdfcf2aa9a13b8e3007d82fa6695e0
Diemale <diego1072@hotmail.com>
2022-12-09 10:04:14 -0300

    main.py comments

--- main.py
+++ main.py
@@ -2,36 +2,47 @@ from fastapi import FastAPI
 import pandas as pd
 import uvicorn
 
+# Import data
 df = pd.read_csv('all_shows.csv')
 
+# create the app
 app = FastAPI()
 
+### Create the functions to make the requests
 
+## Test function
 @app.get("/")
 def read_root(nombre):
     return nombre
 
+## Main functions
+# These functions use type hints to restrict the data type used in the queries (i.e. year: int means the input data
+# type must be an integer. Otherwise, the user will receive an instant error message.
+
 @app.get("/get_max_duration")
 def get_max_duration(year: int, company: str, category: str):
-    if category == "TV Show":
-        DF = df[(df['company'] == company) & (df['release_year'] == year)]
-        dfMax = DF[DF['seasons'] == DF['seasons'].max()]
+    if category == "TV Show":                                               # if input value is "Tv Show"
+        DF = df[(df['company'] == company) & (df['release_year'] == year)]  # filter by company and year
+        dfMax = DF[DF['seasons'] == DF['seasons'].max()]            # create a new DF with only 'max seasons' row
+        # create a return string with the data we need from the last DataFrame
         return f'The TV show with the most seasons in {dfMax.iloc[0][8]} for the year {dfMax.iloc[0][4]} is \
 {dfMax.iloc[0][2]} with {dfMax.iloc[0][6]} seasons.'
 
-    else:
-        DF = df[(df['company']== company) & (df['release_year']== year)]
-        dfMax = DF[DF['duration_mins'] == DF['duration_mins'].max()]
+    else:                                                              # Valid input "Movie"
+        DF = df[(df['company']== company) & (df['release_year']== year)]  # filter by company and year
+        dfMax = DF[DF['duration_mins'] == DF['duration_mins'].max()]      # create a new DF with only 'max seasons' row
+         # create a return string with the data we need from the last DataFrame
         return f'The longest movie in {dfMax.iloc[0][8]} for the year {dfMax.iloc[0][4]} is\
  {dfMax.iloc[0][2]}, it lasts {dfMax.iloc[0][5]} minutes.'
 
 
 @app.get("/get_count_plataform")
 def show_count_two(company: str):
-    grouped = df.groupby(['company'])
-    grouped = grouped['show_type'].value_counts().to_frame()
-    grouped=grouped.rename(columns = {'show_type':'count'})
-    grouped = grouped.reset_index()
+    grouped = df.groupby(['company'])                            # Group data by 'company' column
+    grouped = grouped['show_type'].value_counts().to_frame()    # count grouped values, convert the resulting series to a DataFrame
+    grouped = grouped.rename(columns={'show_type': 'count'})   # rename column to allow an index reset
+    grouped = grouped.reset_index()                            # reset the index to work neatly
+    # Get a return string showing the needed data for every possible company
     if company == "Amazon Prime":
         return f'{grouped.iloc[0][0]} has listed {grouped.iloc[0][2]} movies and {grouped.iloc[1][2]} TV Shows.'
     elif company == "Disney":
@@ -44,38 +55,45 @@ def show_count_two(company: str):
 
 @app.get("/listedin")
 def get_genre_qt(genre: str):
+    # get the sum of every single value from a column with multiple-value cells
     data = df.listed_in.str.get_dummies(sep=', ').sum()
-    data = data.to_frame()
-    data = data.rename(columns={0: 'count'}).reset_index()
-    data.rename(columns={'index': 'genre'},inplace=True)
+    data = data.to_frame()                                  # convert series to a DataFrame
+    data = data.rename(columns={0: 'count'}).reset_index()  # rename column to allow an index reset
+    data.rename(columns={'index': 'genre'}, inplace=True)   # same as above
 
-    dfMyGenre = data[data['genre'] == genre].reset_index(drop=True)
-    myGenreCount =  dfMyGenre.at[0,'count']                         #  genre total
+    dfMyGenre = data[data['genre'] == genre].reset_index(drop=True)  # filter DataFrame by input genre
+    myGenreCount =  dfMyGenre.at[0,'count']                         # save genre total (int)
 
+    # Convert a column with multiple-value cells to a Data-Frame with one column for
+    # every unique value(pretty similar to one-hot-encoding)
     ver = df.listed_in.str.get_dummies(sep=', ')
-    verSeries = ver.pop(genre)
-    mySeries = df['company']
-    verComp = pd.concat([verSeries, mySeries], axis=1)
-    verComp = verComp[verComp[genre] == 1]                        # Filter
-    vCGroup = verComp.groupby('company')
-    ff = vCGroup[genre].value_counts().to_frame()
-    ff = ff.rename(columns={genre:'count'}).reset_index()
-    ff = ff[ff['count'] == ff['count'].max()]
 
+    verSeries = ver.pop(genre)       # create a Series by popping the column with an equal name to the input parameter
+    mySeries = df['company']         # create a company Series from the original DataFrame
+    verComp = pd.concat([verSeries, mySeries], axis=1)      # concatenate both Series
+    verComp = verComp[verComp[genre] == 1]                   # Filter the resulting DataFrame by the input genre
+    vCGroup = verComp.groupby('company')                   # Group by company
+    ff = vCGroup[genre].value_counts().to_frame()    # count grouped values, convert the resulting series to a DataFrame
+    ff = ff.rename(columns={genre:'count'}).reset_index()   # reset the index to work neatly
+    ff = ff[ff['count'] == ff['count'].max()]           # filter the DataFrame to get a new one-row DF with the max-count
+
+    # Return a string with the requested data
     return f'Existen {myGenreCount} registros del genero {genre}. La plataforma con mayor oferta de este genero es \
 {ff.at[0,"company"]} con {ff.at[0,"count"]} series y peliculas.'
 
 
 @app.get("/actor")
-def max_performer(company: str, year: int):    # Cuando lo lleves raw Py tenés que tipar los parámetros company: str  year: int
+def max_performer(company: str, year: int):
     if company == 'Hulu':
-        return 'There are no records for this category in Hulu'
-    else:
-        DF = df[(df['company']== company) & (df['release_year'] == year)]
+        return 'There are not records for this category in Hulu'  # There aren't actor values in Hulu's DataSet
+    else:                                                                   # For the other companies
+        DF = df[(df['company'] == company) & (df['release_year'] == year)]  # filter by company and year
+        # get the sum of every single value from a column with multiple-value cells
         DF = DF.cast.str.get_dummies(sep=', ').sum()
-        DF = DF.to_frame().reset_index().rename(columns={"index": "genre", 0:'total'})
-        DF = DF[DF.total == DF.total.max()]
+        DF = DF.to_frame().reset_index().rename(columns={"index": "genre", 0:'total'})  # rename columns
+        DF = DF[DF.total == DF.total.max()]           # filter the DataFrame to get a new one-row DF with the max-count
 
+        # Return a string with the requested data
         return f'The performer with the most apparitions in {company} in {year} is {DF.iloc[0][0]}, with a total of\
  {DF.iloc[0][1]} movies and tv shows.'
 
